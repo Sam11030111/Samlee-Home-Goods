c86eddf91791b1a555df966d805161688f3938fe
Sam11030111 <sam86110306@gmail.com>
2024-11-10 14:40:48 -0600

    add feature for searching products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,20 @@
 class ProductsController < ApplicationController
   def index
-    if params[:category_id]
-      @products = Product.where(category_id: params[:category_id])
-    else
-      @products = Product.all
+    @categories = Category.all
+    @products = Product.all
+
+    # Filter by category if a category is selected
+    if params[:category_id].present?
+      @products = @products.where(category_id: params[:category_id])
     end
+
+    # Filter by search query if provided
+    if params[:query].present?
+      @products = @products.where("name ILIKE ?", "%#{params[:query]}%")
+    end
+
+    # Paginate the products
+    @products = @products.page(params[:page]).per(5)
   end
 
   def show
