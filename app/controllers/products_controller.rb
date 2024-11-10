8c239b2dc55386be813715c260cb1c336c77cee3
Sam11030111 <sam86110306@gmail.com>
2024-11-10 13:16:15 -0600

    add tabs for filtering categories in the products page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,13 @@
 class ProductsController < ApplicationController
-    def index
-      @products = Product.all()
+  def index
+    if params[:category_id]
+      @products = Product.where(category_id: params[:category_id])
+    else
+      @products = Product.all
     end
+  end
 
-    def show
-      @product = Product.find(params[:id])
-    end
+  def show
+    @product = Product.find(params[:id])
+  end
 end
